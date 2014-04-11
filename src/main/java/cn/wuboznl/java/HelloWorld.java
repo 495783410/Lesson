6e329a73fc9e42c84a34d538e3a1f54bc2b59bbf
495783410 <495783410@qq.com>
2014-04-11 11:41:51 +0800

    测试merge

--- src/main/java/cn/wuboznl/java/HelloWorld.java
+++ src/main/java/cn/wuboznl/java/HelloWorld.java
@@ -1,8 +1,13 @@
 package cn.wuboznl.java;
 public class HelloWorld {
-	public void sayHello()
+	public void sayHello2()
 	{
-		System.out.println("Hello");
+		System.out.println("HelloWorldHello");
+	}
+	public String sayHello()
+	{
+		System.out.println("HelloMerge");
+		return "Hello";
 	}
 
 }
