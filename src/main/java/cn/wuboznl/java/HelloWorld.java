3f0b0f0f71896b9e4af1c2089e0c3e47dad42ae6
495783410 <495783410@qq.com>
2014-04-11 11:46:41 +0800

    尝试Merge冲突

--- src/main/java/cn/wuboznl/java/HelloWorld.java
+++ src/main/java/cn/wuboznl/java/HelloWorld.java
@@ -1,13 +1,12 @@
 package cn.wuboznl.java;
 public class HelloWorld {
-	public void sayHello2()
+	public void sayHello()
 	{
 		System.out.println("HelloWorldHello");
 	}
-	public String sayHello()
+	public String sayHello2()
 	{
 		System.out.println("HelloMerge");
 		return "Hello";
 	}
-
 }
